5a06c4e5c8eed6fad4d5a4a2c7b6d6452a4fe62f
Mateusz Majchrzak <matemajch@gmail.com>
2022-11-01 10:38:59 +0100

    added initializeMiddleware function

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,19 @@
 import express, { Express } from "express";
+import helmet from "helmet";
+import cors from "cors";
+import morgan from "morgan";
+import compression from "compression";
 
 const app: Express = express();
 
-const initializeMiddleware = () => {};
+const initializeMiddleware = () => {
+  app.use(helmet());
+  app.use(cors());
+  app.use(morgan("common"));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(compression());
+};
 
 const start = (): void => {
   try {
