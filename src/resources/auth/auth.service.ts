410bd9fec7913c652b73d308824a8fbf584bc085
Mateusz Majchrzak <36929127+maaajo@users.noreply.github.com>
2022-11-21 09:52:23 +0100

    added more refresh token services

--- src/resources/auth/auth.service.ts
+++ src/resources/auth/auth.service.ts
@@ -1,10 +1,28 @@
 import { Prisma } from "@prisma/client";
 import { db } from "../../db/prisma.db";
 
-const saveRefreshToken = async (
-  refreshTokenData: Prisma.RefreshTokenCreateArgs["data"]
-) => {
+type TRefreshTokenUniqueValues = Prisma.RefreshTokenFindUniqueArgs["where"];
+type TRefreshTokenInputData = Prisma.RefreshTokenCreateArgs["data"];
+
+const saveRefreshToken = async (refreshTokenData: TRefreshTokenInputData) => {
   return await db.refreshToken.create({ data: refreshTokenData });
+};
+
+const getRefreshTokenUser = async (findValues: TRefreshTokenUniqueValues) => {
+  return await db.refreshToken.findUnique({
+    where: findValues,
+    select: { user: true },
+  });
+};
+
+const updateRefreshToken = async (
+  findValues: TRefreshTokenUniqueValues,
+  dataToUpdate: TRefreshTokenInputData
+) => {
+  return await db.refreshToken.update({
+    where: findValues,
+    data: dataToUpdate,
+  });
 };
 
 export { saveRefreshToken };
